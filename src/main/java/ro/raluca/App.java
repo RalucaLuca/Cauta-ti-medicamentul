7910b4eeea8eab00c5d309584efa2a78ef0ed16f
Raluu <Raluu@Ralu.Raluu>
2018-05-10 15:48:43 +0300

    Adaugare afisare medicamentii si farmacii

--- src/main/java/ro/raluca/App.java
+++ src/main/java/ro/raluca/App.java
@@ -1,5 +1,8 @@
 package ro.raluca;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -9,8 +12,33 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws SQLException
     {
-        SpringApplication.run(App.class, args);       
+    	// Afisare farmacii
+    			Farmacie conexiuneFarmacii = new Farmacie();
+    			conexiuneFarmacii.getDataFromFarmacie();
+    			System.out.println();
+
+    			// Afisare sediile unei farmacii
+    			// conex_farmacii.SediileUneiFarmacii("catena");
+
+    			// AFISARE SEDIU_FARMACIE
+    			SediuFarmacie conexiuneSediiFarmacie = new SediuFarmacie();
+    			conexiuneSediiFarmacie.getDataFromSediuFarmacie();
+    			System.out.println();
+
+    			Medicament conexiuneMedicamente = new Medicament();
+    			// Afisare medicamente din DB
+    			// conex_medicament.conectareDB();
+    			// List<Medicament> medicamenteFromDB = conexiuneMedicamente.getDateFromDB();
+    			// conexiuneMedicamente.displayMedicamente(medicamenteFromDB);
+
+    			// AFISARE MEDICAMENTE DE PE SITE
+    			conexiuneMedicamente.conectareSiteNomenclator();
+    			List<Medicament> medicamenteFromSite = conexiuneMedicamente.getDataFromSite();
+    			conexiuneMedicamente.displayMedicamente(medicamenteFromSite);    	
+    	
+    			SpringApplication.run(App.class, args);
+        
     }
 }
